a8e51ff7f30fc6203a8376fcd1b38bc89600542f
Josh Tillinghast <jwtill@icloud.com>
2022-01-21 11:53:03 -0800

    Trouble shoot presidence behavior when loop is introduced

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 // Business Logic
-
 function beepBoop(num) {
   if (isNaN(num)) {
     return "Please enter a number";
@@ -7,26 +6,19 @@ function beepBoop(num) {
 
   let numString = "";
   numString = numString.concat(num);
-  console.log("numString Value:", num);
   let output = [];
 
   for (i=0; i<=parseInt(numString); i++) {
-    console.log("value of i:", i);
-
     if (i === 0) {
-      console.log("Includes 0");
       output.push("0");
     }
-    else if (i === 3) {
-      console.log("Includes 3");
+    else if ((i === 3) || (i.toString().includes("3"))) {
       output.push("Won't you be my neighbor");
     }  
-    else if (i === 2) {
-      console.log("Includes 2");
+    else if ((i === 2) || (i.toString().includes("2"))){
       output.push("Boop");
     }
-    else if (i === 1) {
-      console.log("Includes 1");
+    else if ((i === 1) || (i.toString().includes("1"))) {
       output.push("Beep");
     }
     else 
